cea9150e195fb26d6ad4613e24c42b69603adc0e
SimonovAlex <SimonovAlex13@gmail.com>
2020-03-11 01:07:48 +0200

    update
    add cross-env

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 
 app.use(express.json({extended: true}));
 app.use('/api', require('./routes/invoice.router'));
 
 const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'prod'){
+    app.use('/', express.static(path.join(__dirname, 'client', "build")));
+
+    app.use('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 
 async function start() {
     app.listen(PORT, () => {
