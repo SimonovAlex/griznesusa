3a247ac129cb1aa13832e3252b543e1f2941a706
SimonovAlex <SimonovAlex13@gmail.com>
2020-05-02 13:51:26 +0300

    fix rout

--- app.js
+++ app.js
@@ -45,10 +45,6 @@ app.use((req, res, next) => {
 
 if (process.env.NODE_ENV === 'prod'){
     app.use('/', express.static(path.join(__dirname, 'client', "build")));
-
-    app.use('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
 }
 
 
